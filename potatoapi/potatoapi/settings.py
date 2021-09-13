36c4db6b5085a220a5c4af5ee9f15bfc1cc17ee5
Sreedhar <sreedharks2003@gmail.com>
2021-09-13 20:48:31 +0530

    Add pagination Settings

--- potatoapi/potatoapi/settings.py
+++ potatoapi/potatoapi/settings.py
@@ -91,7 +91,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
          'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+
 }
 
 # Password validation
