dade164c04e52df2137b776b403d54cc12420761
Sreedhar <sreedharks2003@gmail.com>
2021-09-15 18:04:01 +0530

    Add post count to fields

--- potatoapi/accounts/serializers.py
+++ potatoapi/accounts/serializers.py
@@ -1,7 +1,7 @@
 
 from rest_framework import serializers
 from .models import Profile
-from social.models import Follow
+from social.models import Follow,Post
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
@@ -11,10 +11,11 @@ class ProfileSerializer(serializers.ModelSerializer):
     id=serializers.SerializerMethodField()
     follower_count=serializers.SerializerMethodField()
     following_count=serializers.SerializerMethodField()
+    post_count=serializers.SerializerMethodField()
     #mutual_friends=serializers.SerializerMethodField()
     class Meta :
         model =Profile
-        fields=['id','user','follower_count','following_count','bio','verified','pic']
+        fields=['id','user','follower_count','following_count','post_count','bio','verified','pic']
     def get_user(self,obj):
         return obj.user.username
     def get_id(self,obj):
@@ -28,7 +29,10 @@ class ProfileSerializer(serializers.ModelSerializer):
         user=obj.user.id
         follow=Follow.objects.filter(follower=user).count() #follower=user defines people we are following
         return follow
-
+    def get_post_count(self,obj):
+        user=obj.user.id
+        post_num=Post.objects.filter(user=user).count()
+        return post_num
 class SignupSerializers(serializers.ModelSerializer):
     password2=serializers.CharField(style={'input_text':'password'},write_only=True)
     class Meta:
