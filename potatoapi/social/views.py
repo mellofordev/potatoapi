ce4415ff55d3e85c021dd4035905d2b24d411eb8
Sreedhar k.s <53270409+mellofordev@users.noreply.github.com>
2021-10-19 20:21:54 +0530

    Update views.py

--- potatoapi/social/views.py
+++ potatoapi/social/views.py
@@ -1,12 +1,12 @@
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 from rest_framework.response import Response
-from .models import Follow, Post,Comment,Like
+from .models import Follow, Post,Comment,Like,Notification
 from rest_framework.decorators import api_view
 from ipaddr import client_ip
 from rest_framework.authentication import TokenAuthentication
 from accounts.models import Profile
-from .serializers import NewPostSerializers, PostSerializers,CommentSerializers,CommentViewSerializers
+from .serializers import NewPostSerializers, PostSerializers,CommentSerializers,CommentViewSerializers,NotificationSerializers
 from accounts.serializers import ProfileSerializer
 from django.contrib.auth.models import User
 from rest_framework.pagination import LimitOffsetPagination
@@ -28,19 +28,20 @@ def home_api(request):
         feeds_for_user=paginnation_classes.paginate_queryset(feeds_for_user,request)
         size_of_feed_query=len(feeds_for_user)
         paginnation_classes.max_limit=size_of_feed_query
+        response=[]
         if size_of_feed_query==0:
-            #suggest some users 
-            response={}
+            #suggest some users
+
             get_minimal_user_profile_data=Profile.objects.filter(blocked='False')
             #get_profile_asc =get_minimal_user_profile_data.order_by('joined').reverse()
             for i in get_minimal_user_profile_data:
-                
+
                 profile_serializer=ProfileSerializer(i)
                 val=i.id
-                response[val]=profile_serializer.data
-            return Response({'notification':'Follow some users to get started','follow':response})
+                response.append(profile_serializer.data)
+            #return Response({'follow':response})
         feeds_after_serialization=PostSerializers(feeds_for_user,many=True,context={'request':request})
-        return paginnation_classes.get_paginated_response({'feed':feeds_after_serialization.data})
+        return paginnation_classes.get_paginated_response({'feed':feeds_after_serialization.data,'follow':response})
         #return Response({'feed':feeds_after_serialization.data})
 
 @api_view(['POST'])
@@ -53,10 +54,10 @@ def newpost(request):
         user_Data =User.objects.get(username=get_user)
 
         user =Post(user=user_Data)
-        
-        
+
+
         serializers =NewPostSerializers(user.user,data=request.data)
-    
+
         if serializers.is_valid():
             serializers.save()
             return Response({'post': 'Successfully posted'})
@@ -66,7 +67,7 @@ def newpost(request):
 def post_view_of_user(request,slug):
     authentication=[TokenAuthentication]
     user=request.user
-    
+    pagination=LimitOffsetPagination()
     if slug.isdigit():
         return Response({'error':'Type error Id provided.'})
     elif slug.isalpha:
@@ -75,17 +76,31 @@ def post_view_of_user(request,slug):
             get_username=get_username.id
         except ObjectDoesNotExist:
             return Response({'error':'User does not exits'})
-        
+
     if user.is_anonymous:
         return Response({'error':'Token not provided'})
 
     get_posts=Post.objects.filter(user=get_username)
-    serializers=PostSerializers(get_posts,many=True)
-    if request.method=='GET':
-        profile_url='/accounts/api/profile/{}'.format(slug)
-        return Response({'posts':serializers.data,'profile-url':profile_url})
+    get_posts=get_posts.order_by('date_posted').reverse()
+    feeds=pagination.paginate_queryset(get_posts,request)
+    size_of_feed=len(feeds)
+    pagination.max_limit=size_of_feed
 
-    return Response({'error':serializers.error})
+    serializers=PostSerializers(feeds,many=True,context={'request':request})
+    return pagination.get_paginated_response({'feed':serializers.data})
+
+    #return Response({'error':serializers.error})
+@api_view(['GET'])
+def post_of_request_user(request):
+    authentication_classes=[TokenAuthentication]
+    pagination=LimitOffsetPagination()
+    get_user=request.user
+    get_posts=Post.objects.filter(user=get_user.id)
+    get_posts=get_posts.order_by('date_posted').reverse()
+    feeds=pagination.paginate_queryset(get_posts,request)
+    pagination.max_limit=len(feeds)
+    serializer=PostSerializers(feeds,many=True,context={'request':request})
+    return pagination.get_paginated_response({'feed':serializer.data})
 @api_view(['GET'])
 def follow_api(request,slug):
     authentication_classes =[TokenAuthentication]
@@ -94,17 +109,18 @@ def follow_api(request,slug):
         return Response({'error':'Token not provided'})
     try:
         get_username=User.objects.get(username=slug)
-        #also get the id 
+        #also get the id
         #get_user_id =Profile.objects.get(user=get_username)
     except ObjectDoesNotExist:
         return Response({'error':'Profile does not exits'})
     if str(get_user)==str(get_username):
         return Response({'error':'Follow not allowed'})
     obj,create=Follow.objects.get_or_create(following=get_username,follower=request.user)
-    get_profile_data=Profile.objects.get(uuid_all=get_username.id)
-    print(get_profile_data.id)
-    serializers=ProfileSerializer(get_profile_data)
-    return Response({'profile':serializers.data})
+    objs,created=Notification.objects.get_or_create(fromuser=get_user,touser=get_username,_type='Following',is_read=False)
+    #get_profile_data=Profile.objects.get(uuid_all=get_username.id)
+
+    #serializers=ProfileSerializer(get_profile_data)
+    return Response({'follow':'Followed successfully'})
 @api_view(['GET'])
 def followers_api(request,slug):
     authentication_classes=[TokenAuthentication]
@@ -115,31 +131,75 @@ def followers_api(request,slug):
         user=User.objects.get(username=slug)
     except ObjectDoesNotExist:
         return Response({'error':'Profile not found'})
-    get_followers_list=Follow.objects.filter(following=user.id)    
+    get_followers_list=Follow.objects.filter(following=user.id)
     response=[]
     for i in get_followers_list:
         user_id=User.objects.get(username=i)
         profile_data=Profile.objects.get(uuid_all=user_id.id)
-        profile_after_serialization=ProfileSerializer(profile_data)
+        profile_after_serialization=ProfileSerializer(profile_data,context={'request':request})
         response.append(profile_after_serialization.data)
-        
+
+    return Response({'followers':response})
+@api_view(['GET'])
+def following_api(request,slug):
+    authentication_classes=[TokenAuthentication]
+    get_user=request.user
+    if get_user.is_anonymous:
+        return Response({'error':'Token not provided'})
+    try:
+        user=User.objects.get(username=slug)
+    except ObjectDoesNotExist:
+        return Response({'error':'Profile not found'})
+    get_followers_list=Profile.objects.filter(user__following__follower=user)
+    response=[]
+    for i in get_followers_list:
+        user_id=User.objects.get(username=i)
+        profile_data=Profile.objects.get(uuid_all=user_id.id)
+        profile_after_serialization=ProfileSerializer(profile_data,context={'request':request})
+        response.append(profile_after_serialization.data)
+
     return Response({'followers':response})
 @api_view(['GET'])
 def like_api(request,slug):
     authentication_classes=[TokenAuthentication]
     get_user=request.user
+    get_post_id_=0
     if get_user.is_anonymous:
         return Response({'error':'No Token provided.'})
     try:
         get_post=Post.objects.get(post_short_link=slug)
-        print(get_post)
+        get_post_id_=get_post.id
     except ObjectDoesNotExist:
         return Response({'error':'Post not found'})
     obj,created=Like.objects.get_or_create(post=get_post,user=get_user,liked=True)
+
     if obj:
+        objs,create=Notification.objects.get_or_create(fromuser=get_user,touser=get_post.user,_type='Liked',reference_id=get_post_id_,is_read=False)
         return Response({'liked':'Added to likes'})
     else:
-        
+
+        return Response({'error':'Something unexcepted happended Try again Later'})
+@api_view(['DELETE'])
+def unlike_api(request,slug):
+    authentication_classes=[TokenAuthentication]
+    get_user=request.user
+    get_post_id_=0
+    if get_user.is_anonymous:
+        return Response({'error':'No Token provided.'})
+    try:
+        get_post=Post.objects.get(post_short_link=slug)
+        get_post_id_=get_post.id
+    except ObjectDoesNotExist:
+        return Response({'error':'Post not found'})
+    like_objs=Like.objects.get(post=get_post,user=get_user)
+    get_Notification=Notification.objects.get(fromuser=get_user,touser=get_post.user,_type='Liked',reference_id=get_post_id_)
+    if like_objs.liked==True:
+        like_objs.delete()
+
+        get_Notification.delete()
+        return Response({'liked':'Deleted likes'})
+    else:
+
         return Response({'error':'Something unexcepted happended Try again Later'})
 
 @api_view(['POST'])
@@ -149,11 +209,13 @@ def comment_create_api(request,slug):
     if get_user.is_anonymous:
         return Response({'error':'No token provided'})
     serializers=CommentSerializers(data=request.data,context={'request':request,'slug':slug})
+    get_post=Post.objects.get(post_short_link=slug)
     if serializers.is_valid():
         serializers.save()
-        return Response({'comment':'Comment post successfully'})
+        obj,create=Notification.objects.get_or_create(fromuser=get_user,touser=get_post.user,_type='Commented',reference_id=get_post.id)
+        return Response({'comments':'Comment post successfully'})
     else:
-        return Response({'error':serializers.error})
+        return Response({'error':serializers.data})
 @api_view(['GET'])
 def comment_view_api(request,slug):
     authentication_class=[TokenAuthentication]
@@ -167,7 +229,7 @@ def comment_view_api(request,slug):
         for i in get_comment:
             serializer=CommentViewSerializers(i)
             response.append(serializer.data)
-        return Response({'comments':response})
+        return Response({'comment':response})
     except ObjectDoesNotExist:
         return Response({'error':'No Post found'})
 
@@ -179,9 +241,47 @@ def unfollow_api(request,slug):
         return Response({'error':'No Token provided'})
     try:
         get_profile=User.objects.get(username=slug)
-        
+
         get_follow=Follow.objects.get(following=get_profile,follower=get_user)
+        get_Notification=Notification.objects.get(fromuser=get_user,touser=get_profile,_type='Following')
+        get_Notification.delete()
         get_follow.delete()
         return Response({'unfollowed':'unfollowed successfully'})
     except ObjectDoesNotExist:
-        return Response({'error':'No profile found'})        
+        return Response({'error':'No profile found'})
+@api_view(['GET'])
+def notification_view_api(request):
+    authentication_class=[TokenAuthentication]
+    get_user=request.user
+    if get_user.is_anonymous:
+        return Response({'error':'No token provided'})
+
+    try:
+        get_notification=Notification.objects.filter(touser=get_user.id)
+
+    except ObjectDoesNotExist:
+        return Response({'notification':'No notification'})
+    if len(get_notification)!=0:
+        for i in get_notification:
+            i.is_read=True
+            i.save()
+
+    response=[]
+    for i in get_notification:
+        serializer=NotificationSerializers(i)
+        response.append(serializer.data)
+    return Response({'notification':response})
+
+@api_view(['GET'])
+def notification_count_api(request):
+    authentication=[TokenAuthentication]
+    get_user=request.user
+    if get_user.is_anonymous:
+        return Response({'error':'Token not provided'})
+    try:
+        notification_count=Notification.objects.filter(touser=get_user,is_read=False).count()
+    except ObjectDoesNotExist:
+        return Response({'notificationacount':0})
+    return Response({'notificationcount':notification_count})
+
+
