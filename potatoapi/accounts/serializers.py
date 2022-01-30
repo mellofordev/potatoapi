aa2e33db68597a9207f49f1d5e8373283fcda1e2
sreedhar <sreedharks2003@gmail.com>
2022-01-30 23:12:10 +0530

    new badges serializer

--- potatoapi/accounts/serializers.py
+++ potatoapi/accounts/serializers.py
@@ -12,24 +12,30 @@ class ProfileSerializer(serializers.ModelSerializer):
     follower_count=serializers.SerializerMethodField()
     following_count=serializers.SerializerMethodField()
     post_count=serializers.SerializerMethodField()
+    badges =serializers.SerializerMethodField()
     #mutual_friends=serializers.SerializerMethodField()
-    
+    follower,following,post_num=0,0,0
+    badges_list =[
+        'https://i.pinimg.com/170x/c6/1d/b5/c61db53bf914bcc3df879c09517ef6fb.jpg',
+        'https://toppng.com/uploads/preview/yan-cat-practice-vector-by-cheesefaceman1-on-deviantart-nyan-cat-emoji-gif-11563236761nimgongmvn.png',
+        'https://img.favpng.com/5/0/15/nyan-cat-computer-icons-desktop-wallpaper-png-favpng-KPZZ22ZH3FzTtbxD2s5UG1VhZ.jpg'
+        ]
     class Meta :
         model =Profile
-        fields=['id','user','follower_count','following_count','post_count','bio','verified','pic']
+        fields=['id','user','follower_count','following_count','post_count','bio','verified','pic','badges']
     def get_user(self,obj):
         return obj.user.username
     def get_id(self,obj):
         return  obj.user.id
     def get_follower_count(self,obj):
         user=obj.user.id
-        follow=Follow.objects.filter(following=user).count() #following=user defines people who follow us
+        follower=Follow.objects.filter(following=user).count() #following=user defines people who follow us
         
-        return follow
+        return follower
     def get_following_count(self,obj):
         user=obj.user.id
-        follow=Follow.objects.filter(follower=user).count() #follower=user defines people we are following
-        return follow
+        following=Follow.objects.filter(follower=user).count() #follower=user defines people we are following
+        return following
     def get_post_count(self,obj):
         user=obj.user.id
         post_num=Post.objects.filter(user=user).count()
@@ -39,6 +45,16 @@ class ProfileSerializer(serializers.ModelSerializer):
         get_request_user=self.context.get('request').user
         get_list=Follow.objects.filter(following=get_user)
         return get_list'''
+    def get_badges(self,obj):
+        badges_list_Response =[]
+
+        if self.post_num>=10:
+            badges_list_Response.append(self.badges_list[2])
+        if self.follower<=3 :
+            badges_list_Response.append(self.badges_list[1])
+        if self.following<=3:
+            badges_list_Response.append(self.badges_list[0])
+        return badges_list_Response    
 class SignupSerializers(serializers.ModelSerializer):
     password2=serializers.CharField(style={'input_text':'password'},write_only=True)
     class Meta:
